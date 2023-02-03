ce6833bbd244338d05e69aaa3f702f2d9ef25a4e
Elbert van de Put <elbert@vention.nl>
2023-02-03 17:11:21 +0100

    opengl: Finally get something working, appareantly modern opengl stuff is required.

--- README.md
+++ README.md
@@ -10,13 +10,21 @@ A weather router for sail boats.
 ## TODO
 - Be able to view charts
   - Plot a part of an image using opengl and glarea
-  - Figure out where to download charts
+    - [x] Create a glarea
+    - [ ] Display an image in the opengl area
+  - Figure out where to download charts:
+    - https://www.naturalearthdata.com/ 1:10m physical vectors
+      - Load shapefile using https://github.com/OSGeo/shapelib
+    - https://www.gebco.net/data_and_products/gridded_bathymetry_data/ (probably not usefull)
+    - https://visibleearth.nasa.gov/collection/1484/blue-marble?page=2 
   - Plot charts
   - Plot with correct coordinates on the chart
 - Plot boats
   - Plot boat sprite at correct position and heading
 - Show wind
   - Load wind data
+    - Get data from (http://www.tecepe.com.br/wiki/index.php?title=NOAAWinds)
+    - Decode using https://github.com/LouJo/grib2dec or other library
   - Show wind on map
 - Model boat movement
   - With different wind speeds and angles
@@ -28,3 +36,4 @@ A weather router for sail boats.
 
 ## Dependencies
 - libgtk-4-dev
+- libglew-dev
--- main.cpp
+++ main.cpp
@@ -1,53 +1,118 @@
+#include <GL/glew.h>
 #include <gtk/gtk.h>
-#include <GL/gl.h>
 
-static gboolean on_realize (GtkGLArea *area) {
-    // We need to make the context current if we want to
-    // call GL API
-    gtk_gl_area_make_current (area);
+static struct {
+    GLuint vbo = 0;
+    GLuint vao = 0;
+    GLuint shader_programme = 0;
+} viewport_state;
 
-    // If there were errors during the initialization or
-    // when trying to make the context current, this
-    // function will return a GError for you to catch
-    if (gtk_gl_area_get_error (area) != NULL) {
-        g_printerr("Error while realizing chart drawing area\n");
-        return FALSE;
-    }
+float points[] = {
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f};
 
-    return TRUE;
-}
+const char* vertex_shader =
+        "#version 400\n"
+        "in vec3 vp;"
+        "void main() {"
+        "  gl_Position = vec4(vp, 1.0);"
+        "}";
 
-static gboolean on_unrealize (GtkGLArea *area) {
-
-    return TRUE;
-}
+const char* fragment_shader =
+        "#version 400\n"
+        "out vec4 frag_colour;"
+        "void main() {"
+        "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+        "}";
 
 static void start_simulation(gpointer user_data) {
     g_print("Starting simulation!\n");
 }
 
-static gboolean render(GtkGLArea* area, GdkGLContext* context) {
+static gboolean on_realize(GtkGLArea* area) {
+    // We need to make the context current if we want to
+    // call GL API
+    gtk_gl_area_make_current(area);
+
+    // start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // get version info
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION);   // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+
+    // If there were errors during the initialization or
+    // when trying to make the context current, this
+    // function will return a GError for you to catch
+    if (gtk_gl_area_get_error(area) != NULL) {
+        g_printerr("Error while realizing chart drawing area\n");
+        return FALSE;
+    }
+
+    // Generate buffers
+    glGenBuffers(1, &viewport_state.vbo);
+    glGenVertexArrays(1, &viewport_state.vao);
+
+    // Compile shaders
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    viewport_state.shader_programme = glCreateProgram();
+    glAttachShader(viewport_state.shader_programme, fs);
+    glAttachShader(viewport_state.shader_programme, vs);
+    glLinkProgram(viewport_state.shader_programme);
+
+    return TRUE;
+}
+
+static gboolean on_unrealize(GtkGLArea* area) {
+
+    return TRUE;
+}
+
+static void resize(GtkGLArea* self, gint width, gint height, gpointer user_data) {
+    gtk_gl_area_make_current(self);
+
+    g_print("GL Area has size (%d, %d)\n", width, height);
+
+    // Setup the viewport
+    glViewport(0, 0, (GLsizei) width, (GLsizei) height);
+    //glMatrixMode(GL_PROJECTION);
+    //glLoadIdentity();
+    //gluOrtho2D(0, width, 0, height);
+}
+
+static gboolean render_gl(GtkGLArea* area, GdkGLContext* context) {
     // inside this function it's safe to use GL; the given
     // GdkGLContext has been made current to the drawable
     // surface used by the `GtkGLArea` and the viewport has
     // already been set to be the size of the allocation
+    gtk_gl_area_make_current(area);
 
     // we can start by clearing the buffer
-    glClearColor(0, 0.5, 0, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.1, 0.2, 0.5, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // draw your object
-    // Drawing is done by specifying a sequence of vertices.  The way these
-    // vertices are connected (or not connected) depends on the argument to
-    // glBegin.  GL_POLYGON constructs a filled polygon.
-    glBegin(GL_POLYGON);
-    glColor3f(1, 0, 0); glVertex3f(-0.6, -0.75, 0.5);
-    glColor3f(0, 1, 0); glVertex3f(0.6, -0.75, 0);
-    glColor3f(0, 0, 1); glVertex3f(0, 0.75, 0);
-    glEnd();
+    glBindBuffer(GL_ARRAY_BUFFER, viewport_state.vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
 
-    // Flush drawing command buffer to make drawing happen as soon as possible.
-    glFlush();
+    glBindVertexArray(viewport_state.vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, viewport_state.vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glUseProgram(viewport_state.shader_programme);
+    glBindVertexArray(viewport_state.vao);
+    // draw points 0-3 from the currently bound VAO with current in-use shader
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // we completed our drawing; the draw commands will be
     // flushed at the end of the signal emission chain, and
@@ -86,16 +151,19 @@ static void on_activate(GtkApplication* app, gpointer user_data) {
     GtkWidget* gl_area = gtk_gl_area_new();
     gtk_widget_set_hexpand(gl_area, true);
     gtk_widget_set_vexpand(gl_area, true);
+    gtk_widget_set_visible(gl_area, true);
     gtk_gl_area_set_auto_render(GTK_GL_AREA(gl_area), true);
-    g_signal_connect(gl_area, "render", G_CALLBACK(render), NULL);
+    g_signal_connect(gl_area, "render", G_CALLBACK(render_gl), NULL);
     g_signal_connect(gl_area, "realize", G_CALLBACK(on_realize), NULL);
     g_signal_connect(gl_area, "unrealize", G_CALLBACK(on_unrealize), NULL);
+    g_signal_connect(gl_area, "resize", G_CALLBACK(resize), NULL);
     gtk_box_append(GTK_BOX(box), gl_area);
 
     gtk_widget_show(window);
 }
 
 int main(int argc, char** argv) {
+
     // Create a new application
     GtkApplication* app = gtk_application_new("com.example.GtkApplication",
                                               G_APPLICATION_DEFAULT_FLAGS);
